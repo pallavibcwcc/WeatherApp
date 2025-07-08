4625a8290ac059e10b4289f7b9c9d3052db4b59e
Pallavi Kumari <pallavi.brty@gmail.com>
2025-07-08 17:38:33 +0530

    weather api

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Weather from './Weather'
+const App = () => {
+  return(
+    <div>
+<Weather/>
+    </div>
+  )
+}
+export default App;
---
+++ src/Weather.jsx
@@ -0,0 +1,12 @@
+const Weather =()=>{
+    return(
+        <div>
+<main>
+    <div classname="search_bar">
+<input classname="search_box"  placeholder=""/>
+    </div>
+</main>
+        </div>
+    )
+}
+export default Weather;
