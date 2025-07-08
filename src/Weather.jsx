21d8320911447df60b52df0baf11231c53563652
Pallavi Kumari <pallavi.brty@gmail.com>
2025-07-08 18:27:06 +0530

    data

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,9 +1,37 @@
+import {useState} from 'react'
+const api = {
+    key:"7cc88d95e41ba70147315f46b3a52e94",
+    base:"https://api.openweathermap.org/data/2.5/"
+}
 const Weather =()=>{
+    const[Query,setQuery]=useState('');
+    const[weather,setWeather]=useState({});
+    const search = evt =>{
+        if(evt.key==='Enter'){
+fetch(`${api.base}weather?q=${Query}&units=metric&APPID=${api.key}`)
+        .then(res=>res.json())
+        .then(result=>{
+            setWeather(result);
+            setQuery('');
+        })
+    }
+
+    }
     return(
         <div>
 <main>
-    <div classname="search_bar">
-<input classname="search_box"  placeholder=""/>
+    <div className="search_bar">
+<input className="search_box"  placeholder="search...."
+value={Query}
+onChange={e=>setQuery(e.target.value)}
+onKeyPress={search}
+/>
+    </div>
+    <div className="loacation_box">
+        <div className="loacation">
+            
+        </div>
+
     </div>
 </main>
         </div>
